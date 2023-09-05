9a39c7cdbe3eac8e9e039662369ca353d0e45981
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-09-05 15:52:30 +0530

    Added Update and Delete Logs to Front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,14 @@ import SearchBar from "./searchBar";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import DisplayRes from "./DisplayRes";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 function App() {
   const [search, setSearch] = useState("");
   const [hits, setHits] = useState(0);
   const [data, setData] = useState([]);
+  const [updating, setUpdating] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get(
@@ -33,14 +36,60 @@ function App() {
     };
   }, [search]);
 
+  async function updateLogs() {
+    const response = await axios.post(`http://localhost:8000/index`);
+    if (response.status === 200) {
+      setUpdating(false);
+    }
+  }
+
+  const [deleting, setDeleting] = useState(false);
+
+  async function deleteLogs() {
+    const response = await axios.delete(`http://localhost:8000/index`);
+    if (response.status === 200) {
+      setDeleting(false);
+    }
+  }
+
   return (
     <div className="App">
-      <div className="flex flex-col w-full min-h-screen justify-start pt-10 items-center">
-        <div className="flex flex-row w-full justify-center items-center">
+      <div className="flex flex-col w-full min-h-screen justify-start pt-10 items-center gap-3">
+        <div className="flex flex-row w-full justify-center items-center gap-3">
           <SearchBar setSearch={setSearch} search={search}></SearchBar>
-          <p className="text-2xl border border-blue-300 p-2 rounded-md ml-3">
+          <p className="text-2xl border border-blue-300 p-2 rounded-md">
             {hits}
           </p>
+        </div>
+        <div className="flex flex-row w-1/2 gap-3">
+          <LoadingButton
+            size="large"
+            color="primary"
+            className="w-1/2"
+            onClick={() => {
+              setUpdating(true);
+              updateLogs();
+            }}
+            loading={updating}
+            loadingIndicator="Updating..."
+            variant="contained"
+          >
+            <span>Update Logs</span>
+          </LoadingButton>
+          <LoadingButton
+            size="large"
+            color="error"
+            className="w-1/2"
+            onClick={() => {
+              setDeleting(true);
+              deleteLogs();
+            }}
+            loading={deleting}
+            loadingIndicator="Deleting..."
+            variant="contained"
+          >
+            <span>Delete Logs</span>
+          </LoadingButton>
         </div>
         <DisplayRes data={data}></DisplayRes>
       </div>
